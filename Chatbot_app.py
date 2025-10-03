9b835ae5b59f1ccf6cb3c13205f608fa593e3523
MohammoD2 <ibr500037@gmail.com>
2025-10-03 12:05:52 +0600

    adding the chatbot v2

--- Chatbot_app.py
+++ Chatbot_app.py
@@ -15,7 +15,7 @@ st.title("Welcome TO AllOfTech's Chatbot")
 # --- Sidebar Settings ---
 with st.sidebar:
     # --- Logo Section ---
-    logo_url = "images\Logo.png"  # 🔸 Replace with your actual logo URL or local path
+    logo_url = "https://alloftech-org.github.io/AllOfTech/images/Logo.png"  # 🔸 Replace with your actual logo URL or local path
 
     st.markdown(
         f"""
